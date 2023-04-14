7d3afed1d5ac615d94fac8ce2a83819751871927
igor.epsardinha <igor.sardinha@outlook.com>
2023-04-13 22:04:38 -0300

    CSS

--- controle_bancario/bancario.php
+++ controle_bancario/bancario.php
@@ -5,12 +5,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Controle Bancário</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+
+  <div class="container">
+  <h3>💰 CONTROLE BANCÁRIO 💰</h3>
+    <div id="content">
     <?php if ($_SERVER["REQUEST_METHOD"] == "POST") {
-      echo "<h3>CONTROLE BANCÁRIO:</h3>";
       $nome_correntista = $_POST["nome"];
-      echo "Nome do correntista: " . $nome_correntista;
+      echo "<b>Nome do correntista:</b> " . $nome_correntista;
       $banco = $_POST["banco"];
       if ($banco == "santander") {
         $banco = "Banco Santander";
@@ -27,24 +31,35 @@
       } else {
         $banco = "--";
       }
-      echo "<br> Nome do banco: " . $banco;
+      echo "<br> <b>Nome do banco:</b> " . $banco;
       $numero_conta = $_POST["numero_conta"];
-      echo "<br> Número do conta: " . $numero_conta;
+      $agencia = $_POST["agencia"];
+      echo "<br> <b>Agência:</b> " . $agencia;
+      echo "<br> <b>Número do conta:</b> " . $numero_conta;
       $tipo_conta = $_POST["tipo_conta"];
       if ($tipo_conta == "on") {
         $tipo_conta = "Conta Especial";
       } else {
         $tipo_conta = "Conta Comum";
       }
-      echo "<br> Tipo de conta: " . $tipo_conta;
+      echo "<br> <b>Tipo de conta: </b>" . $tipo_conta;
       $data_abertura = $_POST["data_abertura"];
-      echo "<br> Data de abertura: " . $data_abertura;
+      echo "<br> <b>Data de abertura: </b>" . $data_abertura;
       $saldo_inicial = $_POST["saldo_inicial"];
-      echo "<br> Sala inicial: " . $saldo_inicial;
       $deposito = $_POST["deposito"];
-      echo "<br> Deposito: " . $deposito;
+      echo "<div class='valores'>";
+      echo "<div id='saldo'>";
+      echo "<b>Saldo inicial: </b><br>R$" . $saldo_inicial;
+      echo "</div>";
+      echo "<div id='deposito'>";
+      echo "<b>Deposito: </b><br>R$" . $deposito;
+      echo "</div>";
+      echo "</div>";
     } else {
       echo "<h3>É necessário criar um cadastro</h3>";
+      echo "<a href='index.html'>Cadastro de conta</a>";
     } ?>
+    </div>
+    </div>
 </body>
 </html>
