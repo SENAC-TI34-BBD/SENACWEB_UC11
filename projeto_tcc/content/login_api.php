e84ec4f2bd416abae8ea1755643a346aaca18458
igor.epsardinha <igor.sardinha@outlook.com>
2023-05-09 19:25:58 -0300

    Upload

--- projeto_tcc/content/login_api.php
+++ projeto_tcc/content/login_api.php
@@ -2,40 +2,37 @@
 header("Content-Type:application/json");
 if (
   isset($_POST["username"]) &&
-  $_POST["username"] != "" &&
-  $_POST["password"] &&
-  $_POST["password"] != ""
+  $_POST["username"] != ""
+  //* $_POST["password"] && $_POST["password"] != "" //
 ) {
-  include "db.php";
+  include "./config/db.php";
   $username = $_POST["username"];
-  $password = $_POST["password"];
+  //$password = $_POST["password"];
   $result = mysqli_query(
     $con,
-    "SELECT * FROM `users_login` WHERE username=$username and pass=$password LIMIT 1"
+    "SELECT * FROM `users_login` WHERE username ='$username'"
   );
   if (mysqli_num_rows($result) > 0) {
     $row = mysqli_fetch_array($result);
     $user_id = $row["user_id"];
     $name = $row["name"];
-    $picture = $row["profile_pic"];
-    $response_code = $row["response_code"];
-    $response_desc = $row["response_desc"];
-    response($user_id, $name, $picture, $response_code, $response_desc);
+    $picture = $row["picture"];
+    $username = $row["username"];
+    response($user_id, $name, $picture, $username);
     mysqli_close($con);
   } else {
-    response(null, null, null, 200, "No Record Found");
+    response(null, null, null, null);
   }
 } else {
-  response(null, null, null, 400, "Invalid Request");
+  response(null, null, null, null);
 }
 
-function response($user_id, $name, $picture, $response_code, $response_desc)
+function response($user_id, $name, $picture, $username)
 {
   $response["user_id"] = $user_id;
   $response["name"] = $name;
   $response["picture"] = $picture;
-  $response["response_code"] = $response_code;
-  $response["response_desc"] = $response_desc;
+  $response["username"] = $username;
 
   $json_response = json_encode($response);
   echo $json_response;
