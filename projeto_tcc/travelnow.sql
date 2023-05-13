6c7267c0a5bc145f82a164f9c3cbda48f8bbd89a
igor.epsardinha <igor.sardinha@outlook.com>
2023-05-12 21:01:43 -0300

    sql

--- projeto_tcc/travelnow.sql
+++ projeto_tcc/travelnow.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 11, 2023 at 11:59 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Tempo de geração: 13-Maio-2023 às 02:00
+-- Versão do servidor: 10.4.25-MariaDB
+-- versão do PHP: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,48 +18,81 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `travelnow`
+-- Banco de dados: `travelnow`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `usuarios`
+-- Estrutura da tabela `users_login`
+--
+
+CREATE TABLE `users_login` (
+  `user_id` int(20) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `username` varchar(16) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `picture` varchar(256) NOT NULL,
+  `active` int(1) NOT NULL DEFAULT 1,
+  `profile_type` int(1) NOT NULL DEFAULT 1,
+  `user_hash` varchar(100) NOT NULL,
+  `create_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `users_login`
+--
+
+INSERT INTO `users_login` (`user_id`, `name`, `username`, `email`, `password`, `picture`, `active`, `profile_type`, `user_hash`, `create_at`, `updated_at`) VALUES
+(1, 'Igor Sardinha', 'igor.sardinha', 'igor.sardinha@outlook.com', 'MTIzNDU2', 'https://i.imgur.com/11fE8Wf.png', 1, 2, '1', '2023-05-09 19:05:51', '2023-05-09 20:19:18');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios`
 --
 
 CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(220) NOT NULL,
-  `email` varchar(220) NOT NULL,
-  `senha` varchar(220) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `id` int(20) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `sobrenome` varchar(50) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `telefone` varchar(32) NOT NULL,
+  `senha` varchar(256) NOT NULL,
+  `picture` varchar(256) DEFAULT NULL,
+  `create_at` datetime DEFAULT current_timestamp(),
+  `updated_at` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `usuarios`
---
-
-INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`) VALUES
-(2, 'igor.sardinha', 'igor.sardinha@outlook.com', '$2y$10$N3TSzBpjifKOE2GEDoBhvOwr6gci538CoAjRpy6PWpBEXMbzI2b5y');
-
---
--- Indexes for dumped tables
+-- Índices para tabelas despejadas
 --
 
 --
--- Indexes for table `usuarios`
+-- Índices para tabela `users_login`
+--
+ALTER TABLE `users_login`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Índices para tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT de tabelas despejadas
 --
 
 --
--- AUTO_INCREMENT for table `usuarios`
+-- AUTO_INCREMENT de tabela `users_login`
 --
-ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+ALTER TABLE `users_login`
+  MODIFY `user_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
